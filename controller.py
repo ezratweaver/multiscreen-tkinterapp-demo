6a22c96fa51d688869c2e5e626622766fcf96ec7
ezratweaver <101545981+ezratweaver@users.noreply.github.com>
2023-04-26 14:12:13 -0500

    Controller File

---
+++ controller.py
@@ -0,0 +1,22 @@
+from secondscreen import SecondScreen, FirstScreen, window
+
+first_screen = FirstScreen()
+second_screen = SecondScreen()
+
+class Controller:
+    def firstscreen_button_check():
+        if first_screen.first_screen_button_pressed:
+            first_screen.first_screen_button_pressed = False
+            first_screen.hide_canvas()
+            second_screen.show_canvas()
+        window.after(10, Controller.firstscreen_button_check)
+
+    def secondscreen_button_check():
+        if second_screen.second_screen_button_pressed:
+            second_screen.second_screen_button_pressed = False
+            second_screen.hide_canvas()
+            first_screen.show_canvas()
+        window.after(10, Controller.secondscreen_button_check)
+
+Controller.firstscreen_button_check()
+Controller.secondscreen_button_check()
