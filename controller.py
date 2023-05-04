017e2be34ff9c976af101c0bef6db718dfee98f8
ezratweaver <ezratweaver@gmail.com>
2023-05-03 21:06:21 -0500

    Updated refresh times to increase efficiency

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
-from secondscreen import SecondScreen, FirstScreen, window
+from firstscreen import FirstScreen
+from secondscreen import SecondScreen, window
 
 first_screen = FirstScreen()
 second_screen = SecondScreen()
@@ -9,14 +10,14 @@ class Controller:
             first_screen.first_screen_button_pressed = False
             first_screen.hide_canvas()
             second_screen.show_canvas()
-        window.after(10, Controller.firstscreen_button_check)
+        window.after(70, Controller.firstscreen_button_check)
 
     def secondscreen_button_check():
         if second_screen.second_screen_button_pressed:
             second_screen.second_screen_button_pressed = False
             second_screen.hide_canvas()
             first_screen.show_canvas()
-        window.after(10, Controller.secondscreen_button_check)
+        window.after(70, Controller.secondscreen_button_check)
 
 Controller.firstscreen_button_check()
 Controller.secondscreen_button_check()
